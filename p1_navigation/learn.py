25ca8173e4a4fddf6300cb3e8448062b08baf48a
Joe Booth <joe@joebooth.com>
2020-02-08 18:51:49 -0800

    save when a new high score

--- p1_navigation/learn.py
+++ p1_navigation/learn.py
@@ -85,6 +85,8 @@ def dqn(n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.99
     scores = []                        # list containing scores from each episode
     scores_window = deque(maxlen=100)  # last 100 scores
     eps = eps_start                    # initialize epsilon
+    high_score = 5
+    high_scores_window = deque(maxlen=3)    # last 3 scores
     for i_episode in range(1, n_episodes+1):
         env_info = env.reset(train_mode=True)[brain_name]
         state = env_info.vector_observations[0]
@@ -105,14 +107,15 @@ def dqn(n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.99
                 break 
         scores_window.append(score)       # save most recent score
         scores.append(score)              # save most recent score
+        high_scores_window.append(score)       # save most recent score
         eps = max(eps_end, eps_decay*eps) # decrease epsilon
         print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
         if i_episode % 100 == 0:
             print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
-        if np.mean(scores_window)>=20.0:
-            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+        if np.mean(high_scores_window)>high_score:
+            high_score = np.mean(high_scores_window)
+            print('\rNew high score on episode {:d}\t High Score: {:.2f}'.format(i_episode-3, high_score))
             torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
-            break
     return scores
 
 scores = dqn()
