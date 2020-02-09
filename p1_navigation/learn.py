5a25fc9417e4935ebc5362545c47104de9134f07
Joe Booth <joe@joebooth.com>
2020-02-08 18:04:41 -0800

    fix: was only selecting random actions - doh

--- p1_navigation/learn.py
+++ p1_navigation/learn.py
@@ -90,7 +90,8 @@ def dqn(n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.99
         state = env_info.vector_observations[0]
         score = 0
         for t in range(max_t):
-            action = np.random.randint(action_size)        # select an action
+            # action = np.random.randint(action_size)        # select random action
+            action = agent.act(state, eps)                 # select an action
             env_info = env.step(action)[brain_name]        # send the action to the environment
             next_state = env_info.vector_observations[0]   # get the next state
             reward = env_info.rewards[0]                   # get the reward
@@ -108,7 +109,7 @@ def dqn(n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.99
         print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
         if i_episode % 100 == 0:
             print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
-        if np.mean(scores_window)>=200.0:
+        if np.mean(scores_window)>=20.0:
             print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
             torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
             break
@@ -133,7 +134,7 @@ for i in range(5):
     state = env.reset()
     for j in range(200):
         action = agent.act(state)
-        env.render()
+        # env.render()
         state, reward, done, _ = env.step(action)
         if done:
             break 
